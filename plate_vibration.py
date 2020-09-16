e74c19bb2475f53555b1936e1cf5c8fd8bdff4ed
Xelxeim <xelxeim@gmail.com>
2020-09-16 19:05:17 +0300

    Some code refactoring, adding gif-generating

--- plate_vibration.py
+++ plate_vibration.py
@@ -2,52 +2,15 @@ import pylab
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from matplotlib import cm
+import os
+import imageio
 
+a = 10
+s = 0.003
+h = 0.1
 
-def modify_last_arr(array, a, h, s):
-    for j in range(1, 6):
-        for k in range(1, 6):
-            array[2][j][k] = (a ** 2) * (s ** 2) * (array[1][j][k + 1] + array[1][j][k - 1] + array[1][j + 1][k] + \
-                                                    array[1][j - 1][k] - 4 * array[1][j][k]) / (h ** 2) + 2 * \
-                             array[1][j][k] - array[0][j][k]
-    return array
-
-
-def swap_arr(array):
-    for j in range(1, 6):
-        for k in range(1, 6):
-            array[0][j][k] = array[1][j][k]
-            array[1][j][k] = array[2][j][k]
-    return array
-
-
-def make_graph(array):
-    zgrid = np.array(array)
-    xgrid = np.array([
-        [0, 1, 2, 3, 4, 5, 6],
-        [0, 1, 2, 3, 4, 5, 6],
-        [0, 1, 2, 3, 4, 5, 6],
-        [0, 1, 2, 3, 4, 5, 6],
-        [0, 1, 2, 3, 4, 5, 6],
-        [0, 1, 2, 3, 4, 5, 6],
-        [0, 1, 2, 3, 4, 5, 6]
-    ])
-    ygrid = np.array([
-        [0, 0, 0, 0, 0, 0, 0],
-        [1, 1, 1, 1, 1, 1, 1],
-        [2, 2, 2, 2, 2, 2, 2],
-        [3, 3, 3, 3, 3, 3, 3],
-        [4, 4, 4, 4, 4, 4, 4],
-        [5, 5, 5, 5, 5, 5, 5],
-        [6, 6, 6, 6, 6, 6, 6]
-    ])
-    fig = pylab.figure()
-    axes = Axes3D(fig)
-    axes.plot_surface(xgrid, ygrid, zgrid, cmap=cm.viridis)
-    axes.set_zlim(-5, 5)
-    filename = f'graph{counter}'
-    pylab.savefig(filename, bbox_inches='tight')
-
+fig = pylab.figure()
+axes = Axes3D(fig)
 
 array = [
     [
@@ -79,15 +42,62 @@ array = [
     ]
 ]
 
-a = 10
-s = 0.003
-h = 0.1
-counter = 0
-flag = True
-while flag:
-    array = modify_last_arr(array, a, h, s)
+if os.path.exists('frames'):
+    pass
+else: os.mkdir('frames')
+save_path = os.path.abspath('frames')
+print(save_path)
+
+
+def modify_last_arr(array):
+    for j in range(1, 6):
+        for k in range(1, 6):
+            array[2][j][k] = (a ** 2) * (s ** 2) * (array[1][j][k + 1] + array[1][j][k - 1] + array[1][j + 1][k] + \
+                                                    array[1][j - 1][k] - 4 * array[1][j][k]) / (h ** 2) + 2 * \
+                             array[1][j][k] - array[0][j][k]
+    return array
+
+
+def swap_arr(array):
+    for j in range(1, 6):
+        for k in range(1, 6):
+            array[0][j][k] = array[1][j][k]
+            array[1][j][k] = array[2][j][k]
+    return array
+
+
+def make_graph(array, counter):
+    zgrid = np.array(array)
+    xgrid = np.array([
+        [0, 1, 2, 3, 4, 5, 6],
+        [0, 1, 2, 3, 4, 5, 6],
+        [0, 1, 2, 3, 4, 5, 6],
+        [0, 1, 2, 3, 4, 5, 6],
+        [0, 1, 2, 3, 4, 5, 6],
+        [0, 1, 2, 3, 4, 5, 6],
+        [0, 1, 2, 3, 4, 5, 6]
+    ])
+    ygrid = np.array([
+        [0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 2, 2, 2, 2],
+        [3, 3, 3, 3, 3, 3, 3],
+        [4, 4, 4, 4, 4, 4, 4],
+        [5, 5, 5, 5, 5, 5, 5],
+        [6, 6, 6, 6, 6, 6, 6]
+    ])
+    axes.plot_surface(xgrid, ygrid, zgrid, cmap=cm.viridis)
+    axes.set_zlim(-5, 5)
+    filename = f'{save_path}/graph{counter}'
+    pylab.savefig(filename, bbox_inches='tight')
+
+
+for i in range(25):
+    array = modify_last_arr(array)
     array = swap_arr(array)
-    make_graph(array[2])
-    counter += 1
-    if counter >= 25:
-        flag = False
+    make_graph(array[2], i)
+
+images = []       
+for i in range(25):
+    images.append(imageio.imread(f'{save_path}/graph{i}.png'))
+imageio.mimsave('graph.gif', images)

3b7c4b960fab088750aee1cf391566d957ab4851
Xelxeim <xelxeim@gmail.com>
2020-10-11 19:08:14 +0300

    add initial data and processing conditions

--- lab_3/fading_hesitations.py
+++ lab_3/fading_hesitations.py
@@ -1,2 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+m = 5
+k1 = 3
+k2 = 3
+x = 4
+y = 0
+x1 = 2
+y1 = 3
+t = 0
+p = 0.3
+s = 0.5
+x2 = 0
+y2 = 0
+array = [
+    [0, 0, 0, 2, 3, 4, 0]
+]
+
+iterations = int(input('Input number of iterations: '))
+while True:
+    mode = input(f'Choose mode of program:\n 1 - without fading\n 2 - with fading\n')
+    if mode == "1" or mode == "2":
+        break
+    else:
+        print('Wrong mode')
+        continue

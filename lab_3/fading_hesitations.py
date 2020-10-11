db4b0e53e1cea1703a0859e1803c163ba9fb402a
Xelxeim <xelxeim@gmail.com>
2020-10-11 19:10:00 +0300

    Add main function, some dirty code ._.

--- lab_3/fading_hesitations.py
+++ lab_3/fading_hesitations.py
@@ -17,6 +17,20 @@ array = [
     [0, 0, 0, 2, 3, 4, 0]
 ]
 
+def count_values(mode, t, x1, y1, x, y, p):
+    if mode == "1":
+        x2 = -k1 / m * x
+        y2 = -k2 / m * y
+    else:
+        x2 = -k1 / m * x - p * x1
+        y2 = -k2 / m * y - p * y1
+    t = t + s
+    x1 = x1 + x2 * s
+    y1 = y1 + y2 * s
+    x = x + x1 * s
+    y = y + y1 * s
+    return [t, x2, y2, x1, y1, x, y]
+
 iterations = int(input('Input number of iterations: '))
 while True:
     mode = input(f'Choose mode of program:\n 1 - without fading\n 2 - with fading\n')
@@ -25,3 +39,7 @@ while True:
     else:
         print('Wrong mode')
         continue
+
+for i in range(iterations):
+    array.append(count_values(mode, t, x1, y1, x, y, p))
+    t,x2,y2,x1,y1,x,y = count_values(mode, t, x1, y1, x, y, p)
